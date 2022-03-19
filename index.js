8f49c0d9c862923f6e5a5cb22b1d97d87cd14205
TheWisePigeon <zozozozeph@gmail.com>
2022-03-19 12:32:40 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ const express = require('express')
 const bp = require('body-parser')
 const cloudinary = require('cloudinary')
 const secrets = require('./secrets/secrets')
+const mongoose = require('mongoose')
 const app = express()
+
+
+//databse stuff
+const dbString = `mongodb+srv://${secrets.dbUser}:${secrets.password}@artificial.5ha3m.mongodb.net/artificial?retryWrites=true&w=majority`
+mongoose.connect(dbString, { useNewUrlParser: true } )
+const artworkSchema = new mongoose.Schema({
+    title: String,
+    desc: String,
+    src: String,
+
+})
+
+const Artwork = mongoose.model("Artwork", artworkSchema)
+const artwork = new Artwork({
+    title: "test",
+    desc: "bruh",
+    src: "bruhruuh"
+})
+artwork.save()
 
 app.use(bp.urlencoded({ extended: true }))
 
